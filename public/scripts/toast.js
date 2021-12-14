0acb08bff6b809cc4ed0525b5324fc695fdc9f4a
IsaacAquino21 <emanuel_isaac_aquino@dlsu.edu.ph>
2021-12-14 21:47:36 +0800

    Bug fix for toast
    Fixed bug where the toast-container is blocking the buttons below it.
    
    Fixed by making the div hidden when the toast is closed.

--- public/scripts/toast.js
+++ public/scripts/toast.js
@@ -9,6 +9,9 @@ var toastElement = new bootstrap.Toast(toast, option);
 var toastElement2 = new bootstrap.Toast(toast2, option);
 toastElement.show();
 
-
+toast.addEventListener('hidden.bs.toast', function () {
+    var elem = document.getElementById('toast-container');
+    elem.style.visibility = 'hidden';
+  })
 
 console.log("show toast")
